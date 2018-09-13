5c62522d743d49c91711d0a2ee802b9435ed2af6
alexHooper <alexmohack@gmail.com>
2018-09-13 15:42:42 +0530

    update the env variable secret key

--- tweetme/settings/production.py
+++ tweetme/settings/production.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("TWEETME_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
