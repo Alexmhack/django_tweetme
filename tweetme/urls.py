a8e29c84e9006bc6961e1de168dca7ac36283c25
alexHooper <alexmohack@gmail.com>
2018-09-13 20:45:32 +0530

    remove append to simple addition

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns.append(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
