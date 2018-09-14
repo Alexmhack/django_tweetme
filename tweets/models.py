fa80a5ad922c968bb1ff181a8b94d428b7834703
alexHooper <alexmohack@gmail.com>
2018-09-14 08:01:18 +0530

    add on_delete attribute

--- tweets/models.py
+++ tweets/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Tweet(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	content = models.CharField(max_length=140)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
