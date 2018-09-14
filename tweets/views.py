78c4efeab148e390f81e70bc1cd7ee44bc679e9d
alexHooper <alexmohack@gmail.com>
2018-09-14 17:40:43 +0530

    change path for templates in views

--- tweets/views.py
+++ tweets/views.py
@@ -8,7 +8,7 @@ def tweet_detail_view(request, id=None):
 		'object': object
 	}
 
-	return render(request, 'detail.html', context)
+	return render(request, 'tweets/detail_view.html', context)
 
 
 def tweet_list_view(request):
@@ -17,4 +17,4 @@ def tweet_list_view(request):
 		'objects': objects
 	}
 
-	return render(request, 'list.html', context)
+	return render(request, 'tweets/list_view.html', context)
