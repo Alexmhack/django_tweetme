83fc7e4f73227377614e24fdbb516ff0eab9f00b
alexHooper <alexmohack@gmail.com>
2018-09-14 17:27:53 +0530

    add tweet detail view in views

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tweet
+
+def tweet_detail_view(request, id=None):
+	object = Tweet.objects.get(id=id)
+	context = {
+		'object': object
+	}
+
+	return render(request, 'detail.html', context)
