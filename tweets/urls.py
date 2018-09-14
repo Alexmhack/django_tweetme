003309671e15ffca31e30a2d22256e26bc687a05
alexHooper <alexmohack@gmail.com>
2018-09-14 17:34:13 +0530

    add app_name for tweets

--- tweets/urls.py
+++ tweets/urls.py
@@ -4,6 +4,8 @@ from .views import (
 	tweet_detail_view
 )
 
+app_name = 'tweets'
+
 urlpatterns = [
-	path('<int:id>/detail', tweet_detail_view, name='detail'),
+	path('<int:id>/', tweet_detail_view, name='detail'),
 ]
