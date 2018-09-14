006be315a150f57ae9e787270b57e5c1bdcbe768
alexHooper <alexmohack@gmail.com>
2018-09-14 07:54:09 +0530

    attach user with a tweet object

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 class Tweet(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL)
 	content = models.CharField(max_length=140)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
