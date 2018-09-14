1c5664b5c43d8350b3ead6a8648aa3fbab7e8da7
alexHooper <alexmohack@gmail.com>
2018-09-14 17:38:30 +0530

    add url for list of tweets

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import (
-	tweet_detail_view
+	tweet_detail_view,
+	tweet_list_view,
 )
 
 app_name = 'tweets'
 
 urlpatterns = [
 	path('<int:id>/', tweet_detail_view, name='detail'),
+	path('tweets/', tweet_list_view, name='tweets'),
 ]
