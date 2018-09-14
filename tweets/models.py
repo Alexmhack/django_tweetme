c0de299d5ac03fe7557431ecc826527f5799893b
alexHooper <alexmohack@gmail.com>
2018-09-14 07:38:36 +0530

    add more model fields

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Tweet(models.Model):
-	content = models.TextField()
+	content = models.CharField(max_length=140)
+	updated = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.content
