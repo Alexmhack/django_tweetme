5a26164e51b76d70f93ac69105792c6aba9ec4a1
alexHooper <alexmohack@gmail.com>
2018-09-15 17:08:21 +0530

    use default admin form

--- tweets/admin.py
+++ tweets/admin.py
@@ -6,9 +6,8 @@ from .forms import TweetModelForm
 # admin.site.register(Tweet)
 
 class TweetModelAdmin(admin.ModelAdmin):
-	form = TweetModelForm
-	# class Meta:
-	# 	model = Tweet
-
+	class Meta:
+		model = Tweet
+		
 
 admin.site.register(Tweet, TweetModelAdmin)
