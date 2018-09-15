782bd67ea6d1a6a00b6e9c803886b5e7f82afa32
alexHooper <alexmohack@gmail.com>
2018-09-15 15:50:04 +0530

    replace the admin model form with custom

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 from .models import Tweet
+from .forms import TweetModelForm
 
-admin.site.register(Tweet)
+# admin.site.register(Tweet)
+
+class TweetModelAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Tweet
+		form = TweetModelForm
+
+
+admin.site.register(Tweet, TweetModelForm)
