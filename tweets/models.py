d50e86173fffeca4d5a9cf612b55d3126330afdf
alexHooper <alexmohack@gmail.com>
2018-09-15 16:36:11 +0530

    add validator directly to field

--- tweets/models.py
+++ tweets/models.py
@@ -2,18 +2,26 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
 
+def validate_content(value):
+	content = value
+	if "fuck" in content:
+		raise ValidationError("Cannot have offensive content")
+
+	return value
+
+
 class Tweet(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	content = models.CharField(max_length=140)
+	content = models.CharField(max_length=140, validators=[validate_content])
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.content
 
-	def clean(self, *args, **kwargs):
-		content = self.content
-		if "fuck" in content:
-			raise ValidationError("Cannot have offensive content")
+	# def clean(self, *args, **kwargs):
+	# 	content = self.content
+	# 	if "fuck" in content:
+	# 		raise ValidationError("Cannot have offensive content")
 
-		return super().clean()
+	# 	return super().clean()
