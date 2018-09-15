5c1437fe55a79ccb1dc060ef0d208aa8825635fd
alexHooper <alexmohack@gmail.com>
2018-09-15 16:50:45 +0530

    add url for create view

--- tweets/urls.py
+++ tweets/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
 	TweetDetailView,
 	TweetListView,
+	TweetCreateView,
 )
 
 app_name = 'tweets'
@@ -10,4 +11,5 @@ app_name = 'tweets'
 urlpatterns = [
 	path('<int:pk>/', TweetDetailView.as_view(), name='detail'),
 	path('tweets/', TweetListView.as_view(), name='tweets'),
+	path('create/', TweetCreateView.as_view(), name='create'),
 ]
