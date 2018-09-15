9e41ffe0a62c03b13b7ad0f5dae0919bd3e61de8
alexHooper <alexmohack@gmail.com>
2018-09-15 11:59:54 +0530

    add url for class based list view

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from .views import (
 	TweetDetailView,
-	tweet_list_view,
+	TweetListView,
 )
 
 app_name = 'tweets'
 
 urlpatterns = [
 	path('<int:pk>/', TweetDetailView.as_view(), name='detail'),
-	path('tweets/', tweet_list_view, name='tweets'),
+	path('tweets/', TweetListView.as_view(), name='tweets'),
 ]
