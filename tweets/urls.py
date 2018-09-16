bc9cee0d17c0ffb5dfbb2f6ed4b7128a8ba84aa7
alexHooper <alexmohack@gmail.com>
2018-09-16 13:12:47 +0530

    change url for list view

--- tweets/urls.py
+++ tweets/urls.py
@@ -12,7 +12,7 @@ app_name = 'tweets'
 
 urlpatterns = [
 	path('<int:pk>/', TweetDetailView.as_view(), name='detail'),
-	path('tweets/', TweetListView.as_view(), name='tweets'),
+	path('', TweetListView.as_view(), name='tweets'),
 	path('create/', TweetCreateView.as_view(), name='create'),
 	path('<int:pk>/edit/', TweetUpdateView.as_view(), name='edit'),
 	path('<int:pk>/delete/', TweetDeleteView.as_view(), name='delete'),
