ab27a25270d696100a89464f92a23101a49054d6
alexHooper <alexmohack@gmail.com>
2018-09-16 11:46:19 +0530

    create mixin for checking tweet owner

--- tweets/mixins.py
+++ tweets/mixins.py
@@ -9,3 +9,12 @@ class FormUserNeededMixin(object):
 		else:
 			form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["User must be logged in to continue"])
 			return self.form_invalid(form)
+
+
+class UserTweetMixin(FormUserNeededMixin, object):
+	def form_valid(self, form):
+		if form.instance.user == self.request.user:
+			return super().form_valid(form)
+		else:
+			form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["You are not allowed to change other's content"])
+			return self.form_invalid(form)
