efc821b6acd23fee04bb632c49d02898cd8ea06a
alexHooper <alexmohack@gmail.com>
2018-09-16 09:32:57 +0530

    correct the url for update view

--- tweets/urls.py
+++ tweets/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
 	path('<int:pk>/', TweetDetailView.as_view(), name='detail'),
 	path('tweets/', TweetListView.as_view(), name='tweets'),
 	path('create/', TweetCreateView.as_view(), name='create'),
-	path('edit/', TweetUpdateView.as_view(), name='edit'),
+	path('<int:pk>/edit/', TweetUpdateView.as_view(), name='edit'),
 ]
