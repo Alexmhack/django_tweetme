26d74d4aa5d8447b1c78b0e3a90f5a77c109f539
alexHooper <alexmohack@gmail.com>
2018-09-18 00:03:57 +0530

    root url shows tweet list

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -19,8 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from tweets.views import TweetListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TweetListView.as_view(), name='home'),
     path('tweet/', include('tweets.urls', namespace='tweets')),
 ]
 
