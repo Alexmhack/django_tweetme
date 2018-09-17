f7836439381d696adcc2d6ee2c6287e476332d9d
alexHooper <alexmohack@gmail.com>
2018-09-17 23:25:48 +0530

    create content field with custom classes and placholder

--- tweets/forms.py
+++ tweets/forms.py
@@ -3,6 +3,16 @@ from django import forms
 from .models import Tweet
 
 class TweetModelForm(forms.ModelForm):
+	content = forms.CharField(
+		label='',
+		widget=forms.TextInput(
+			attrs={
+				"placeholder": "Tweet here",
+				"class": "form-control form-control-lg"
+			}
+		)
+	)
+
 	class Meta:
 		model = Tweet
 		fields = ("content",)
