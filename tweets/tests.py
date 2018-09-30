c4f2382414755088779da2a62aa04fef62bd5d91
alexHooper <alexmohack@gmail.com>
2018-09-30 20:45:57 +0530

    make test for url testing

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 from .models import Tweet
 
@@ -14,6 +15,15 @@ class TweetModelTestCase(TestCase):
 			user=User.objects.first(),
 			content='This is some random test content'
 		)
+		test_obj_url = reverse("tweets:detail", kwargs={'pk': test_obj.id})
 
 		self.assertTrue(test_obj.content == 'This is some random test content')
 		self.assertTrue(test_obj.id == 1)
+
+	def test_tweet_url(self):
+		test_obj = Tweet.objects.create(
+			user=User.objects.first(),
+			content='This is some random test content'
+		)
+		test_obj_url = reverse("tweets:detail", kwargs={'pk': test_obj.id})		
+		self.assertEqual(test_obj.get_absolute_url(), test_obj_url)
