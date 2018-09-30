7b2c289df1800001e3c7ee4f2f625a3482af5d12
alexHooper <alexmohack@gmail.com>
2018-09-30 21:29:20 +0530

    create list api view

---
+++ tweets/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+
+from tweets.models import Tweet
+from .serializers import TweetModelSerializer
+
+class TweetListAPIView(generics.ListAPIView):
+	serializer_class = TweetModelSerializer
+
+	def get_queryset(self):
+		return Tweet.objects.all()
