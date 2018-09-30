a2ab411279a5a9acb0a03c2e7b33d3e61fae036b
alexHooper <alexmohack@gmail.com>
2018-09-30 14:07:37 +0530

    add search in url

--- tweets/urls.py
+++ tweets/urls.py
@@ -12,7 +12,7 @@ app_name = 'tweets'
 
 urlpatterns = [
 	path('<int:pk>/', TweetDetailView.as_view(), name='detail'),
-	path('', TweetListView.as_view(), name='list'),
+	path('search/', TweetListView.as_view(), name='list'),
 	path('create/', TweetCreateView.as_view(), name='create'),
 	path('<int:pk>/edit/', TweetUpdateView.as_view(), name='edit'),
 	path('<int:pk>/delete/', TweetDeleteView.as_view(), name='delete'),
