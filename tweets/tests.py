e06965d4c8f6b61111a7888a4eff4e5e1c7a2c6b
alexHooper <alexmohack@gmail.com>
2018-09-30 19:50:31 +0530

    add test for checking tweet content

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Tweet
+
+User = get_user_model()
+
+class TweetModelTestCase(TestCase):
+	def setup(self):
+		random_user = User.objects.create(username='thisistestuser', password='trydjango')
+
+	def test_tweet_item(self):
+		test_obj = Tweet.objects.create(
+			user=random_user,
+			content='This is some random test content'
+		)
+
+		self.assertTrue(test_obj.content == 'This is some random test content')
