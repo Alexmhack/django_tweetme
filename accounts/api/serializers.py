511271fbe76448577a66f43789768ec61769724c
alexHooper <alexmohack@gmail.com>
2018-10-01 08:29:08 +0530

    add follower_count field

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -5,10 +5,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserModelSerializer(serializers.ModelSerializer):
+	follower_count = serializers.SerializerMethodField()
+
 	class Meta:
 		model = User
 		fields = [
 			'username',
 			'first_name',
-			'last_name'
+			'last_name',
+			'follower_count',
 		]
+
+	def get_follower_count(self, obj):
+		return 0
