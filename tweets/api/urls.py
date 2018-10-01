1a7c1a2cf81a918e3385007894dbd0cf1e44ddb5
alexHooper <alexmohack@gmail.com>
2018-10-01 12:22:26 +0530

    add url for create api view

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import TweetListAPIView
+from .views import (
+	TweetListAPIView, TweetCreateAPIView
+)
 
 app_name = 'tweets-api'
 
 urlpatterns = [
-	path('', TweetListAPIView.as_view(), name='list')
+	path('', TweetListAPIView.as_view(), name='list'),
+	path('create/', TweetCreateAPIView.as_view(), name='create'),
 ]
